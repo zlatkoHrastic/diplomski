6329461ac77beae7f9b507c987b38327950ab800
zlatko_hrastic <zlatko.hr8@gmail.com>
2018-04-08 20:13:11 +0200

    Tablice za utrke

---
+++ Diplomski rad baza/Posada.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Posada]
+(
+	[IdRezultat] INT NOT NULL , 
+    [IdVeslac] INT NOT NULL, 
+    CONSTRAINT [FK_Posada_Rezultat] FOREIGN KEY (IdRezultat) REFERENCES [Rezultat](IdRezultat),
+	CONSTRAINT [FK_Posada_Veslac] FOREIGN KEY (IdVeslac) REFERENCES [Veslac](IdVeslac), 
+    PRIMARY KEY ([IdVeslac],[IdRezultat])
+)
