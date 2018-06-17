0efb755854a5916661f61d98177244628105ee4d
zlatko_hrastic <zlatko.hr8@gmail.com>
2018-06-17 13:58:07 +0200

    Sređene relacije

--- Diplomski rad baza/Regata.sql
+++ Diplomski rad baza/Regata.sql
@@ -5,5 +5,5 @@
     [DatumPocetak] DATE NOT NULL, 
     [DatumKraj] DATE NOT NULL, 
     [IdLokacija] INT NOT NULL, 
-    CONSTRAINT [FK_Regata_Staza] FOREIGN KEY (IdLokacija) REFERENCES [Lokacija]([IdLokacija])
+    CONSTRAINT [FK_Regata_Lokacija] FOREIGN KEY (IdLokacija) REFERENCES [Lokacija]([IdLokacija])
 )
