246226701ff658cc1047bd9c9d920a74c7cac51d
zlatko_hrastic <zlatko.hr8@gmail.com>
2018-06-19 18:47:33 +0200

    relacije u tablice

--- Diplomski rad baza/Lokacija.sql
+++ Diplomski rad baza/Lokacija.sql
@@ -2,7 +2,7 @@
 (
 	[IdLokacija] INT NOT NULL PRIMARY KEY IDENTITY, 
     [Naziv] NVARCHAR(MAX) NOT NULL, 
-	GeografskaSirina FLOAT NOT NULL, 
+	[GeografskaSirina] FLOAT NOT NULL, 
     [GeografskaVisina] FLOAT NOT NULL
 
 )
