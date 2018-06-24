0be90acb42af4e39c89f90baab418dd2719dbb2a
zlatko_hrastic <zlatko.hr8@gmail.com>
2018-06-24 21:43:41 +0200

    dodan dijagram

--- Diplomski rad baza/Pogledi/Trening864.sql
+++ Diplomski rad baza/Pogledi/Trening864.sql
@@ -2,4 +2,4 @@
 	AS 	SELECT Trening.IdVeslac,Trening.VrijemeTreninga,DionicaTreninga.BrojDionice, DionicaTreninga.Vrijeme
 	FROM Trening
 	JOIN DionicaTreninga ON DionicaTreninga.IdTrening=Trening.IdTrening
-	WHERE IdTipTreninga=3
+	WHERE IdTipTreninga=1002
