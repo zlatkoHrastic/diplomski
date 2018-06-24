042d9de3af657341ce466a4a7fb09aab93fa6117
zlatko_hrastic <zlatko.hr8@gmail.com>
2018-06-24 21:16:16 +0200

    trening 864

---
+++ Diplomski rad baza/Pogledi/Trening864.sql
@@ -0,0 +1,5 @@
+﻿CREATE VIEW [dbo].[Trening864]
+	AS 	SELECT Trening.IdVeslac,Trening.VrijemeTreninga,DionicaTreninga.BrojDionice, DionicaTreninga.Vrijeme
+	FROM Trening
+	JOIN DionicaTreninga ON DionicaTreninga.IdTrening=Trening.IdTrening
+	WHERE IdTipTreninga=3
